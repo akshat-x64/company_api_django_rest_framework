72352ef5b236e14da88df0dadc371997ff9ce520
Akshat Dwivedi <93469027+akshat-x64@users.noreply.github.com>
2023-08-08 15:09:47 +0530

    Delete Django-rest/companyapi directory

--- Django-rest/companyapi/api/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from api.models import company
-from api.models import employee
-# Register your models here.
-
-admin.site.register(company)
-admin.site.register(employee)
